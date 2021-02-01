97686012e5a13fe44ef897747471d5b0f1af62d3
Alex Robinson <alexandra.scott335@gmail.com>
2021-02-01 11:37:09 -0700

    authentication sad path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,14 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: 'email@example.com')
-    if user.authenticate(params[:password])
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Welcome back #{user.name}"
       redirect_to '/dashboard'
     else
-      flash[:error] = "Email or password incorrect"
+      flash.now[:error] = "Email or password incorrect"
+      render :new
     end
   end
 end
--- spec/features/authentication/user_can_login_spec.rb
+++ spec/features/authentication/user_can_login_spec.rb
@@ -5,16 +5,23 @@ describe "User Authentication" do
     @user = User.create(name: 'Murphy', email: 'email@example.com', password: 'password')
   end
 
-  it "User can login with correct credentials" do
-    # As an unauthenticated user, when I visit `/login`
+  it 'User can login with correct credentials' do
     visit '/login'
-    # and fill in the `email` field with my email and the `password` field with my password,
-    fill_in :email, with: 'user@example.com'
+    fill_in :email, with: 'email@example.com'
     fill_in :password, with: 'password'
-    # then click the `Login` button, I am logged in.
     click_on 'Login'
-    # I am redirected to the `/dashboard` where I see a flash message `Welcome back <my_username>!`
+
     expect(current_path).to eq('/dashboard')
     expect(page).to have_content("Welcome back #{@user.name}")
+  end
+
+  it 'User cannot login with incorrect credentials' do
+    visit '/login'
+    fill_in :email, with: 'user@example.com'
+    fill_in :password, with: 'incorrect password'
+    click_on 'Login'
+
+    expect(current_path).to eq('/login')
+    expect(page).to have_content('Email or password incorrect')
   end
 end
